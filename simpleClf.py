da8336a212e0d8473a7c2ad594d4c8ce46262fa4
hmc-cs-fsouth <fsoutherland@hmc.edu>
2018-04-23 02:07:51 -0700

    Evaluation of ranking strategies

--- simpleClf.py
+++ simpleClf.py
@@ -30,6 +30,16 @@ def get_AUROC(X,y):
     test_std = np.std(test_scores)
     return test_mean, test_std
 
+def get_AUROC_means(X,y):
+    test_means = []
+    test_stds = []
+    for i in range(len(y[0])):
+        y_i = y[:,i]
+        test_mean, test_std = get_AUROC(X,y_i)
+        test_means.append(test_mean)
+        test_stds.append(test_std)  
+    return test_means, test_stds
+
 
 '''
 input: X and y data FOR ONE CLASSIFIER ONLY, k-fold validation sets
@@ -69,12 +79,11 @@ def get_means(X,y):
     train_stds = []
     for i in range(len(y[0])):
         y_i = y[:,i]
-        if not i==8:
-            test_mean, train_mean, test_std, train_std = get_mean(X,y_i)
-            test_means.append(test_mean)
-            test_stds.append(test_std)  
-            train_means.append(train_mean)
-            train_stds.append(train_std)
+        test_mean, train_mean, test_std, train_std = get_mean(X,y_i)
+        test_means.append(test_mean)
+        test_stds.append(test_std)  
+        train_means.append(train_mean)
+        train_stds.append(train_std)
     return tuple(test_means),tuple(train_means),tuple(test_stds),tuple(train_stds)
 
     '''
@@ -85,7 +94,7 @@ def get_means(X,y):
 def plot(test_means,train_means,test_stds,train_stds):
     N = len(test_means)
 
-    ind = np.arange(N)  # the x locations for the groups
+    ind = np.arange(2*N)  # the x locations for the groups
     width = 0.35       # the width of the bars
 
     fig, ax = plt.subplots()
@@ -94,12 +103,11 @@ def plot(test_means,train_means,test_stds,train_stds):
     rects2 = ax.bar(ind + width, train_means, width, color='y', yerr=train_stds)
 
     # add some text for labels, title and axes ticks
-    ax.set_ylabel('Accuracy')
-    ax.set_title('Accuracies by Classifier, Training and Test')
+    ax.set_ylabel('AUROC')
+    ax.set_title('AUROC by Classifier, Frequence and Entropy')
     ax.set_xticks((ind + width) * 0.5)
-    ax.set_xticklabels(('Test Accuraccy', 'Training Accuraccy'))
 
-    ax.legend((rects1[0], rects2[0]), ('Test', 'Train'))
+    ax.legend((rects1[0], rects2[0]), ('Frequency', 'Entropy'))
 
     plt.show()
 
@@ -132,27 +140,44 @@ def main():
     text_array = pre.get_all_text()
     word_counts, length = pre.get_text_dictionary(text_array)
     
-    X = pre.create_X_data(text_array, word_counts, 15, 15)
     y = pre.create_y_data()
 
-    #test_means, train_means, test_stds, train_stds = get_means(X,y)
+    best_test_means = [0 for i in range(len(y[0]))]
+    best_test_stds = [0 for i in range(len(y[0]))]
+    for d in np.arange(1,31,5):
+        X = pre.create_X_data(text_array, word_counts, 15, d)
+        test_means, test_stds = get_AUROC_means(X,y)
+        for i in range(len(y[0])):
+            if test_means[i] > best_test_means[i]:
+                best_test_means[i] = test_means[i]
+                best_test_stds[i] = test_stds[i]
+    print "Did testing by Commonality"
     
-    rVals = [1]
+    rVals = [1,5,10,15,20]
     entropies, bestRndx = getEntropyDict(rVals)
+    print "Have entropies"
 
+    entropy_means = []
+    entropy_stds = []
     for i in range(len(y[0])):
+        print "doing classifier " + str(i)
         wordEntropies = entropies[i]
         #Adjust wordEntropies
         for key in wordEntropies.keys():
             wordEntropies[key] = wordEntropies[key][bestRndx[i]]
         test_means = []
         test_stds = []
-        for d in np.arange(1,100,5):
+        for d in np.arange(1,100,10):
+            print "d = " + str(d)
             X = pre.create_X_data(text_array, wordEntropies, d, rVals[bestRndx[i]], True)
             test_mean, test_std = get_AUROC(X,y[:,i])
             test_means.append(test_mean)
             test_stds.append(test_std)
-        plot2(test_means, test_stds, i)
+        entropy_means.append(max(test_means))
+        entropy_stds.append(max(test_stds))
+
+    plot(tuple(best_test_means), tuple(entropy_means), tuple(best_test_stds), tuple(entropy_stds))
+    
 
 if __name__ == "__main__" :
     main()
